02ed2cdb11153231576e5c6c666f351f324b3d36
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-02 13:27:45 +0500

    Entity.java commit002-variable and method creation
    Declared new variables that are in todo table(index.jsp page). Wrote their getters,setters and toString method.

--- src/main/java/com.Entity/Entity.java
+++ src/main/java/com.Entity/Entity.java
@@ -2,5 +2,46 @@
 package com.entity;
 
 public class Entity {
+  private int id;
+	private String todo;
+	private String timeWindow;
+	private String Status;
+  
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTodo() {
+		return todo;
+	}
+
+	public void setTodo(String todo) {
+		this.todo = todo;
+	}
+
+	public String getTimeWindow() {
+		return timeWindow;
+	}
+
+	public void setTimeWindow(String timeWindow) {
+		this.timeWindow = timeWindow;
+	}
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Entity [id=" + id + ", todo=" + todo + ", timeWindow=" + timeWindow + ", Status=" + Status + "]";
+	}
 
 }
