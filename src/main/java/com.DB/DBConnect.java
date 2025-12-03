a738f5ca7ee5ac42e6fbfd2974a22aacbedd3f3d
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-03 11:13:59 +0500

    DBConnect.java commit001-Connecting
    **Creates and returns MySQL database connection**

--- src/main/java/com.DB/DBConnect.java
+++ src/main/java/com.DB/DBConnect.java
@@ -1,5 +1,23 @@
 package com.DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class DBConnect {
+  private static Connection con;
+  public static Connection getConn() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/todoUsingServlet", "root", "root");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+  
+
+
+
+ 
 
 }
