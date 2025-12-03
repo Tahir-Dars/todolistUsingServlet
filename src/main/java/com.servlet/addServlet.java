1b4b58a9b79f82badb6067bb0ce479ff65d72933
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-03 19:14:23 +0500

    addServlet.java commit002-CodingAdding
    Code for the insertion of tables entries.

--- src/main/java/com.servlet/addServlet.java
+++ src/main/java/com.servlet/addServlet.java
@@ -10,9 +10,16 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/addtodo")
 public class addServlet extends HttpServlet {
 protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+String todo = req.getParameter("todo");
+  String timeWindow = req.getParameter("timeWindow");
+		String status = req.getParameter("status");
+		todoDAO dao = new todoDAO(DBConnect.getConn());
+		boolean f = dao.addtoDo(todo, timeWindow, status);
 
-
-
-  
-}
-  }
+		if (f) {
+			System.out.println("Data Entered Syuccessfully !!");
+		} else {
+			System.out.println("Error !^! ");
+		}
+         }
+               }
