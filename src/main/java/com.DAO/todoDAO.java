8c980a45d06ccd79fe74754aee1573d8605aeaba
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-03 12:37:12 +0500

    todoDAO.java is created
    and added basic coding architecture.

---
+++ src/main/java/com.DAO/todoDAO.java
@@ -0,0 +1,23 @@
+package com.DAO;
+
+public class todoDAO {
+	private Connection con;
+
+	public todoDAO(Connection con) {
+		super();
+		this.con = con;
+	}
+
+	public boolean addtoDo( ) {
+
+		boolean f = false;
+		try {
+          
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return f;
+
+	}
