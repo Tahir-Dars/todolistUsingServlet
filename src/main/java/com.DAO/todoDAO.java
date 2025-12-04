dbebf116613a7682384e25d3e3b126be63dab4d5
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-04 16:30:49 +0500

    todoDAO.java commit001-EntryExtraction
    writing code that extracts out the new entry from Database;

--- src/main/java/com.DAO/todoDAO.java
+++ src/main/java/com.DAO/todoDAO.java
@@ -28,8 +28,30 @@ public class todoDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 		return f;
+	}
+	
+	public List<Entity> getTodo() {
+		List<Entity> list = new ArrayList<Entity>();
+		Entity t = null;
+		try {
+			String sql = "select * from todo";
+			PreparedStatement ps01 = con.prepareStatement(sql);
+			ResultSet rs = ps01.executeQuery();
+			while (rs.next()) {
+				t = new Entity();
+				t.setId(rs.getInt(1));
+				t.setTodo(rs.getString(2));
+				t.setTimeWindow(rs.getString(3));
+				t.setStatus(rs.getString(4));
+				list.add(t);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
 
 	}
 	
