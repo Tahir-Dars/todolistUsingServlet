fddbe3eb7cecc630fb975f783ad791020e71fa8f
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-05 17:33:57 +0500

    UpdateServlet.java commit002-Addition
    added code for values relativeness to db values

--- src/main/java/com.servlet/UpdateServlet.java
+++ src/main/java/com.servlet/UpdateServlet.java
@@ -1,5 +1,25 @@
 package com.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.DAO.todoDAO;
+import com.DB.DBConnect;
+
+@WebServlet("/update")
 public class UpdateServlet {
+  @Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int id = Integer.parseInt(req.getParameter("id"));
+		String todo = req.getParameter("todo");
+		String timeWindow = req.getParameter("timeWindow");
+		String status = req.getParameter("status");
+		todoDAO dao = new todoDAO(DBConnect.getConn());
+	}
 
 }
