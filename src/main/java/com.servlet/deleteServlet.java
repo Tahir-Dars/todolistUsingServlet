8a3ebc8bcf6285e20272ccd86d61ef427cd785c2
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-05 18:21:10 +0500

    deleteServlet.java-httpied
    make it a relative of http

--- src/main/java/com.servlet/deleteServlet.java
+++ src/main/java/com.servlet/deleteServlet.java
@@ -1,6 +1,17 @@
 package com.servlet;
 
-public class deleteServlet {
-	
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/delete")
+public class deleteServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int id = Integer.parseInt(req.getParameter("id"));
+	}
 }
