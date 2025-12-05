d1af04ad37dc457ae6bccd53ba236a38eee2460a
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2025-12-05 18:40:18 +0500

    deleteServlet.java-deletingLogic
    Deleting todo, checking result, redirecting.

--- src/main/java/com.servlet/deleteServlet.java
+++ src/main/java/com.servlet/deleteServlet.java
@@ -13,5 +13,17 @@ public class deleteServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		int id = Integer.parseInt(req.getParameter("id"));
+		todoDAO dao = new todoDAO(DBConnect.getConn());
+
+		boolean f = dao.deletetodo(id);
+		HttpSession session = req.getSession();
+		if (f) {
+			session.setAttribute("sucMsg", "edited successfully");
+			resp.sendRedirect("index.jsp");
+		} else {
+			session.setAttribute("failedMsg", "not edited-Something Went Wrong...");
+			resp.sendRedirect("index.jsp");
+
+		}
 	}
 }
